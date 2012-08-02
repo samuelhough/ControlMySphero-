b6575a03459d39a28918519ecd7b5d467b82fa1e
Sam Hough <stone.hough@gmail.com>
2012-08-01 23:39:08 -0400

    Removed routes for local server

--- local/routes.js
+++ local/routes.js
@@ -1,14 +1,6 @@
 module.exports = function(app, port, _public){
-	app.get('/spherey', function(req, res){
-		res.render('index.jade', {
-			layout: false,
-			server: 'http://localhost:'+(port+1)+'/spherey',
-			port: port+1
-		})
-	})
-	app.get('/js/:file', function(req,res){
-		var fileName = req.params.file;
-		res.sendfile(_public + "/js/"+fileName);
+	app.get('/', function(){
+		res.send("Spherey running")
 	})
 
 }
